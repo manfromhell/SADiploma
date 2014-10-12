c8a4f5f4186b81e2b6cde634d3d1c65bd5244b60
Taras Hrytsko <grasss@ukr.net>
2014-10-12 13:40:34 +0300

    sa fix

--- Diploma/src/main/java/ua/edu/lp/sadiploma/tool/SimAnnealing.java
+++ Diploma/src/main/java/ua/edu/lp/sadiploma/tool/SimAnnealing.java
@@ -1,16 +1,14 @@
 package ua.edu.lp.sadiploma.tool;
 
-import java.util.Random;
-
-public class SimAnnealing extends Thread {
+public class SimAnnealing implements Runnable {
 	private SAConfig config;
 	private Component component;
 	// private static int TYPE;
 
 	// private static BundleType bundleType;
 
+	// private Solution workingSolution;
 	private Solution currentSolution;
-	private Solution workingSolution;
 	private Solution bestSolution;
 
 	public SimAnnealing(Component component, SAConfig config) {
@@ -20,66 +18,29 @@ public class SimAnnealing extends Thread {
 
 	@Override
 	public void run() {
-		boolean useNew = false;
-		currentSolution = new Solution(component, config
-				.getGapsKoef(), config.getRepKoef());
-
-		workingSolution = new Solution(component, config
-				.getGapsKoef(), config.getRepKoef());
-
+		currentSolution = new Solution(component, config.getGapsKoef(),
+				config.getRepKoef());
 		bestSolution = new Solution(component, config.getGapsKoef(),
 				config.getRepKoef());
-
-		currentSolution.computeTargetFunction();
-
-		bestSolution.setSolution(currentSolution);
-		workingSolution.setSolution(currentSolution);
-
+		bestSolution.computeTargetFunction();
 		double temperature = config.getInitialTemperature();
 		while (temperature > config.getFinalTemperature()) {
 			for (int i = 0; i < config.getIterationsAtTemperature(); i++) {
-				useNew = false;
-				workingSolution.randomChange();
-				workingSolution.computeTargetFunction();
-
-				if (workingSolution.getSolutionEnergy() <= currentSolution
-						.getSolutionEnergy()) {
-					useNew = true;
-				} else {
-					double test = new Random().nextDouble(); // Get random value
-																// between 0.0
-																// and 1.0
-					double delta = workingSolution.getSolutionEnergy()
-							- currentSolution.getSolutionEnergy();
-					double calc = Math.exp(-delta / temperature);
-					if (calc > test) {
-						useNew = true;
-					}
-				}
-
-				if (useNew) {
-					useNew = false;
-					currentSolution.setSolution(workingSolution);
-					if (currentSolution.getSolutionEnergy() < bestSolution
-							.getSolutionEnergy()) {
-						bestSolution.setSolution(currentSolution);
-						// System.out.println(bestSolution + "\t\t" +
-						// bestSolution.getSolutionEnergy() +"\t\t"+
-						// temperature);
-
-					}
-				} else {
-					workingSolution.setSolution(currentSolution);
-				}
-
-				if (bestSolution.getSolutionEnergy() == 0) {
-					System.out.print(bestSolution + "\t energy: "
-							+ bestSolution.getSolutionEnergy());
-				}
+				currentSolution.randomChange();
+			}
+			currentSolution.computeTargetFunction();
+			if (currentSolution.getSolutionEnergy() < bestSolution
+					.getSolutionEnergy()) {
+				bestSolution.setSolution(currentSolution);
+				bestSolution.computeTargetFunction();
+				System.out.println("new best energy: "
+						+ currentSolution.getSolutionEnergy());
+			} else {
+				currentSolution.setSolution(bestSolution);
 			}
 			temperature *= config.getAlpha();
 		}
-
+		System.out.println("final temp: "+temperature);
 	}
 
 	/**
